c1b526fbfec8563ddaffe7f9d9b74a7adbf1b0b7
Evgeny Mandrikov <mandrikov@gmail.com>
2010-02-15 00:01:52 +0000

    SONARIDE-7: Add plugin for Eclipse
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@90 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

---
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/SonarPlugin.java
@@ -0,0 +1,20 @@
+package org.sonar.ide.eclipse;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class SonarPlugin extends AbstractUIPlugin {
+
+  @Override
+  public void start(BundleContext context) throws Exception {
+    // TODO Auto-generated method stub
+    super.start(context);
+  }
+
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    // TODO Auto-generated method stub
+    super.stop(context);
+  }
+
+}
