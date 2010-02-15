b22fb2f4245e03c57d92689e9e3a180dd3d67aeb
Evgeny Mandrikov <mandrikov@gmail.com>
2010-02-15 02:52:52 +0000

    SONARIDE-7: Add plugin for Eclipse
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@92 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

---
+++ update-dependencies.bat
@@ -0,0 +1,1 @@
+mvn -o -Dtycho.mode=maven -Pupdate-dependencies clean org.sonatype.tycho:maven-osgi-packaging-plugin:generate-bundle
---
+++ update-dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mvn -o \
+    -Dtycho.mode=maven -Pupdate-dependencies \
+    clean org.sonatype.tycho:maven-osgi-packaging-plugin:generate-bundle
