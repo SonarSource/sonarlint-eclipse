262f5f84ea8b006fac90950eca8ebca9902b83d0
Evgeny Mandrikov <mandrikov@gmail.com>
2010-03-05 13:46:56 +0000

    Update to latest sonar-ws-client.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@138 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.maven.ide.eclipse.sonar/src/org/maven/ide/eclipse/sonar/Activator.java
+++ org.maven.ide.eclipse.sonar/src/org/maven/ide/eclipse/sonar/Activator.java
@@ -3,4 +3,5 @@ package org.maven.ide.eclipse.sonar;
 import org.eclipse.core.runtime.Plugin;
 
 public class Activator extends Plugin {
+  public static final String PLUGIN_ID = "org.maven.ide.eclipse.sonar";
 }
