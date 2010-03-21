27ac005724acf528accd26d4e3a73aabf57b8b05
Evgeny Mandrikov <mandrikov@gmail.com>
2010-03-21 15:30:25 +0000

    SONARIDE-42: Initial commit for integration with Mylyn 3.x
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@196 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

---
+++ org.sonar.ide.eclipse.mylyn3/src/org/sonar/ide/eclipse/mylyn3/Activator.java
@@ -0,0 +1,51 @@
+package org.sonar.ide.eclipse.mylyn3;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ * 
+ * @author Evgeny Mandrikov
+ */
+public class Activator extends AbstractUIPlugin {
+
+  public static final String PLUGIN_ID = "org.maven.ide.eclipse.mylyn3";
+
+  // The shared instance
+  private static Activator plugin;
+
+  /**
+   * The constructor
+   */
+  public Activator() {
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+  }
+
+  /**
+   * Returns the shared instance
+   *
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return plugin;
+  }
+
+}
