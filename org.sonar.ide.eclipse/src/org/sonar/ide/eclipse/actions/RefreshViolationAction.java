e981940053e9366d4d0cecb1806e12d70de33dfa
jerr <l.jeremie@gmail.com>
2010-04-29 08:09:02 +0000

    SONARIDE-27: Allow selection of multiple projects.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@273 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/RefreshViolationAction.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/RefreshViolationAction.java
@@ -1,6 +1,10 @@
 package org.sonar.ide.eclipse.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -32,7 +36,13 @@ public class RefreshViolationAction implements IObjectActionDelegate {
    * @see IActionDelegate#run(IAction)
    */
   public void run(IAction action) {
-    Job job = new RefreshViolationJob((IProject) selection.getFirstElement());
+    List<IProject> projects = new ArrayList<IProject>();
+    for (Object resource : selection.toList()) {
+      IProject project = ((IResource)resource).getProject();
+      if(project.isOpen() && !projects.contains(project))
+        projects.add(project);
+    }
+    Job job = new RefreshViolationJob(projects.toArray(new IProject[projects.size()]));
     job.schedule();
   }
 
