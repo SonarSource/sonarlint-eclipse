344ff81e82be1d13301a0c7909e6ec92a56271c9
jerr <l.jeremie@gmail.com>
2010-05-05 22:29:00 +0000

    SONARIDE-27: manage image to preserve memory.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@318 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/markers/SonarMarkerImageProvider.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/markers/SonarMarkerImageProvider.java
@@ -1,8 +1,10 @@
 package org.sonar.ide.eclipse.markers;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.texteditor.IAnnotationImageProvider;
 import org.sonar.ide.eclipse.SonarPlugin;
 
@@ -10,6 +12,9 @@ import org.sonar.ide.eclipse.SonarPlugin;
  * @author Jérémie Lagarde
  */
 public class SonarMarkerImageProvider implements IAnnotationImageProvider {
+
+  private static Image         image;
+  private static ImageRegistry imageRegistry;
 
   public ImageDescriptor getImageDescriptor(String imageDescritporId) {
     return null;
@@ -20,7 +25,18 @@ public class SonarMarkerImageProvider implements IAnnotationImageProvider {
   }
 
   public Image getManagedImage(Annotation annotation) {
-    return ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/violation.png").createImage();
+    if (image == null) {
+      image = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/violation.png").createImage(); //$NON-NLS-1$
+      String key = Integer.toString(image.hashCode());
+      getImageRegistry(Display.getCurrent()).put(key, image);
+    }
+    return image;
+  }
+
+  private ImageRegistry getImageRegistry(Display display) {
+    if (imageRegistry == null)
+      imageRegistry = new ImageRegistry(display);
+    return imageRegistry;
   }
 
 }
