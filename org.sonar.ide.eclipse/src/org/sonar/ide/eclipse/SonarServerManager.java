19393238fff1bfc336281d22fcc4e6a5eadb40f5
jerr <l.jeremie@gmail.com>
2010-05-06 22:24:46 +0000

    SONARIDE-48: temporary code expects integration with the logger system.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@321 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/SonarServerManager.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/SonarServerManager.java
@@ -1,10 +1,17 @@
 package org.sonar.ide.eclipse;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.widgets.Display;
+import org.sonar.ide.client.SonarClient;
 import org.sonar.ide.eclipse.preferences.PreferenceConstants;
 import org.sonar.ide.shared.DefaultServerManager;
+import org.sonar.ide.ui.ConsoleManager;
 import org.sonar.wsclient.Host;
+import org.sonar.wsclient.Sonar;
+import org.sonar.wsclient.services.Model;
+import org.sonar.wsclient.services.Query;
 
 /**
  * @author Jérémie Lagarde
@@ -14,12 +21,13 @@ public class SonarServerManager extends DefaultServerManager {
   protected SonarServerManager() {
     super(SonarPlugin.getDefault().getStateLocation().makeAbsolute().toOSString());
   }
-  
+
   public Host getDefaultServer() throws Exception {
     String url = SonarPlugin.getDefault().getPreferenceStore().getString(PreferenceConstants.P_SONAR_SERVER_URL);
     return findServer(url);
   }
 
+  @Override
   protected void notifyListeners(final int eventType) {
     for (final IServerSetListener listener : serverSetListeners) {
       Display.getDefault().asyncExec(new Runnable() {
@@ -31,6 +39,39 @@ public class SonarServerManager extends DefaultServerManager {
           }
         }
       });
+    }
+  }
+
+  @Override
+  public Sonar getSonar(String url) {
+    final Host server = createServer(url);
+    return new EclipseSonarClient(server.getHost(), server.getUsername(), server.getPassword());
+  }
+
+  static final class EclipseSonarClient extends SonarClient {
+
+    public EclipseSonarClient(String host) {
+      super(host);
+    }
+
+    public EclipseSonarClient(String host, String username, String password) {
+      super(host, username, password);
+    }
+
+    @Override
+    public <MODEL extends Model> MODEL find(Query<MODEL> query) {
+      ConsoleManager.getConsole().logRequest("find : " + query.getUrl());
+      MODEL model = super.find(query);
+      ConsoleManager.getConsole().logResponse(model != null ? model.toString() : null);
+      return model;
+    }
+
+    @Override
+    public <MODEL extends Model> List<MODEL> findAll(Query<MODEL> query) {
+      ConsoleManager.getConsole().logRequest("find : " + query.getUrl());
+      List<MODEL> result = super.findAll(query);
+      ConsoleManager.getConsole().logResponse("Retrieved " + result.size() + " elements.");
+      return result;
     }
   }
 }
