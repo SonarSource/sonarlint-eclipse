14d9a5de5a924b9762ba7a313efe2908330d83c5
Evgeny Mandrikov <mandrikov@gmail.com>
2010-05-11 03:13:41 +0000

    Add missing license-header
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@352 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/SonarAdapterFactory.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/SonarAdapterFactory.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010 Evgeny Mandrikov
+ *
+ * Sonar-IDE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar-IDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar-IDE; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
 package org.sonar.ide.eclipse.actions;
 
 import org.eclipse.core.resources.IResource;
@@ -22,8 +40,9 @@ public class SonarAdapterFactory implements IAdapterFactory {
       public boolean testAttribute(Object target, String name, String value) {
         if (SONAR_PROJECT.equals(name) && adaptable instanceof IResource) {
           final ProjectProperties properties = ProjectProperties.getInstance((IResource) adaptable);
-          if (properties != null && properties.isProjectConfigured())
+          if (properties != null && properties.isProjectConfigured()) {
             return "true".equals(value);
+          }
         }
         return false;
       }
