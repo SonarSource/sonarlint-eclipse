6645aa011a9b27693975273cccc10052a3952bb7
Evgeny Mandrikov <mandrikov@gmail.com>
2010-06-16 09:16:53 +0000

    SONARIDE-117: Add 'Report Sonar Issue' to help menu
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@502 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

---
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/SendFeedbackAction.java
@@ -0,0 +1,22 @@
+package org.sonar.ide.eclipse.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class SendFeedbackAction implements IWorkbenchWindowActionDelegate {
+
+  public void dispose() {
+  }
+
+  public void init(IWorkbenchWindow window) {
+  }
+
+  public void run(IAction action) {
+  }
+
+  public void selectionChanged(IAction action, ISelection selection) {
+  }
+
+}
