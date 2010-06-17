5cb11019371b7bbf21e0100f9e3d048d7fe455d1
Evgeny Mandrikov <mandrikov@gmail.com>
2010-06-17 22:13:03 +0000

    Split MarkersTest into ViolationsTest and DuplicationsTest
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@507 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse.ui.tests/src/org/sonar/ide/eclipse/ui/tests/ViolationsTest.java
+++ org.sonar.ide.eclipse.ui.tests/src/org/sonar/ide/eclipse/ui/tests/ViolationsTest.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2010 Evgeny Mandrikov
- *
- * Sonar-IDE is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * Sonar-IDE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Sonar-IDE; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
- */
-
 package org.sonar.ide.eclipse.ui.tests;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -31,10 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.sonar.ide.eclipse.SonarPlugin;
 
-/**
- * @author Evgeny Mandrikov
- */
-public class MarkersTest extends UITestCase {
+public class ViolationsTest extends UITestCase {
 
   @Before
   public void setUp() throws Exception {
@@ -55,22 +34,6 @@ public class MarkersTest extends UITestCase {
     bot.sleep(1000 * 30); // TODO Godin: looks like waitForAllBuildsToComplete(); doesn't work
 
     assertThat(getMarkers(projectName, SonarPlugin.MARKER_VIOLATION_ID).length, greaterThan(0));
-  }
-
-  @Test
-  public void testRefreshDuplications() throws Exception {
-    final String projectName = "duplications";
-    importAndConfigureNonMavenProject(projectName);
-
-    final SWTBotTree tree = selectProject(projectName);
-
-    assertThat(getMarkers(projectName, SonarPlugin.MARKER_DUPLICATION_ID).length, is(0));
-
-    ContextMenuHelper.clickContextMenu(tree, "Sonar", "Refresh duplications");
-    waitForAllBuildsToComplete();
-    bot.sleep(1000 * 30); // TODO Godin: looks like waitForAllBuildsToComplete(); doesn't work
-
-    assertThat(getMarkers(projectName, SonarPlugin.MARKER_DUPLICATION_ID).length, greaterThan(0));
   }
 
   private IMarker[] getMarkers(final String projectName, final String markerId) throws Exception {
