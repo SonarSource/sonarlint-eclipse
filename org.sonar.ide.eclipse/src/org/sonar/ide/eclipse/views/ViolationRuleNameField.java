66713c367fa42631b696d139a0db7e2ccd6c9f5f
jerr <l.jeremie@gmail.com>
2010-06-20 00:12:28 +0000

    SONARIDE-119: First version of violations view.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@521 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

---
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/views/ViolationRuleNameField.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010 Evgeny Mandrikov
+ *
+ * Sonar-IDE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar-IDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar-IDE; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.ide.eclipse.views;
+
+import org.eclipse.ui.views.markers.MarkerField;
+import org.eclipse.ui.views.markers.MarkerItem;
+
+/**
+ * @author Jérémie Lagarde
+ */
+public class ViolationRuleNameField extends MarkerField {
+
+  @Override
+  public String getValue(MarkerItem item) {
+    return item.getMarker().getAttribute("rulename", "");
+  }
+
+}
