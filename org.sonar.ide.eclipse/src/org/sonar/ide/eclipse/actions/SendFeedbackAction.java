36c03ca82e9faf4d1becf54db5b784ee1f7f2aa6
Evgeny Mandrikov <mandrikov@gmail.com>
2010-06-23 12:38:51 +0000

    SONARIDE-117: Add 'Report Sonar Issue' to help menu
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@536 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/SendFeedbackAction.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/actions/SendFeedbackAction.java
@@ -1,9 +1,15 @@
 package org.sonar.ide.eclipse.actions;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.sonar.ide.eclipse.SonarPlugin;
 
 public class SendFeedbackAction implements IWorkbenchWindowActionDelegate {
 
@@ -14,6 +20,21 @@ public class SendFeedbackAction implements IWorkbenchWindowActionDelegate {
   }
 
   public void run(IAction action) {
+    IWorkbenchBrowserSupport browserSupport = SonarPlugin.getDefault().getWorkbench().getBrowserSupport();
+    try {
+      URL url = new URL("http://jira.codehaus.org/browse/SONARIDE");
+      if (browserSupport.isInternalWebBrowserAvailable()) {
+        browserSupport.createBrowser(null).openURL(url);
+      } else {
+        browserSupport.getExternalBrowser().openURL(url);
+      }
+    } catch (PartInitException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (MalformedURLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   public void selectionChanged(IAction action, ISelection selection) {
