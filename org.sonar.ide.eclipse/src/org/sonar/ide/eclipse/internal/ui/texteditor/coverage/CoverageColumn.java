79967d8275885607eaa1988ae6ae4e310882e7b5
jerr <l.jeremie@gmail.com>
2010-07-02 00:18:57 +0000

    SONARIDE-60: add "Show sonar coverage" in ruler popup menu.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@548 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/internal/ui/texteditor/coverage/CoverageColumn.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/internal/ui/texteditor/coverage/CoverageColumn.java
@@ -17,13 +17,25 @@
  */
 package org.sonar.ide.eclipse.internal.ui.texteditor.coverage;
 
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.CompositeRuler;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.IVerticalRulerColumn;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 import org.eclipse.ui.texteditor.rulers.AbstractContributedRulerColumn;
+import org.eclipse.ui.texteditor.rulers.IColumnSupport;
+import org.sonar.ide.eclipse.jobs.RefreshCoverageJob;
 
 /**
  * @author Jérémie Lagarde
@@ -31,11 +43,35 @@ import org.eclipse.ui.texteditor.rulers.AbstractContributedRulerColumn;
  */
 public class CoverageColumn extends AbstractContributedRulerColumn {
 
-  private IVerticalRulerColumn delegate;
+  /**
+   * Preference key for showing the coverage ruler.
+   */
+  final static String             SONAR_COVERAGE_RULER = "sonarCoverageRuler";
+  final static String             ID                   = "org.sonar.ide.eclipse.coverageRuler";
+
+  private IVerticalRulerColumn    delegate;
+  private IPropertyChangeListener propertyChangeListener;
 
   public Control createControl(CompositeRuler parentRuler, Composite parentControl) {
     initialize();
     Control control = delegate.createControl(parentRuler, parentControl);
+    showCoverage(isCoverageRulerVisible());
+
+    propertyChangeListener = new IPropertyChangeListener() {
+      public void propertyChange(PropertyChangeEvent event) {
+        if (SONAR_COVERAGE_RULER.equals(event.getProperty())) {
+          showCoverage(isCoverageRulerVisible());
+        }
+      }
+    };
+    final IPreferenceStore store = EditorsUI.getPreferenceStore();
+    store.addPropertyChangeListener(propertyChangeListener);
+    // TODO  need to to it before release ...
+    //    control.addDisposeListener(new DisposeListener() {
+    //      public void widgetDisposed(DisposeEvent e) {
+    //        store.removePropertyChangeListener(propertyChangeListener);
+    //      }
+    //    });
     return control;
   }
 
@@ -63,6 +99,39 @@ public class CoverageColumn extends AbstractContributedRulerColumn {
   private void initialize() {
     if (delegate == null) {
       delegate = new CoverageRulerColumn();
+    }
+  }
+
+  /**
+   * Returns whether the sonar coverage ruler column should be visible according
+   * to the preference store settings.
+   */
+  private boolean isCoverageRulerVisible() {
+    IPreferenceStore store = EditorsUI.getPreferenceStore();
+    return store != null ? store.getBoolean(CoverageColumn.SONAR_COVERAGE_RULER) : false;
+  }
+
+  private void showCoverage(boolean show) {
+    IColumnSupport columnSupport = (IColumnSupport) getEditor().getAdapter(IColumnSupport.class);
+    if (show && delegate != null) {
+      columnSupport.setColumnVisible(getDescriptor(), true);
+      final Job job = new RefreshCoverageJob((AbstractDecoratedTextEditor) getEditor());
+      job.schedule();
+    } else {
+      columnSupport.setColumnVisible(getDescriptor(), false);
+      getEditor().getAdapter(IResource.class);
+      try {
+        final IAnnotationModel model = getEditor().getDocumentProvider().getAnnotationModel(getEditor().getEditorInput());
+        for (Iterator iterator = model.getAnnotationIterator(); iterator.hasNext();) {
+          Annotation annotation = (Annotation) iterator.next();
+          if (annotation.getType().equals("org.sonar.ide.eclipse.fullCoverageAnnotationType")
+              || annotation.getType().equals("org.sonar.ide.eclipse.partialCoverageAnnotationType")
+              || annotation.getType().equals("org.sonar.ide.eclipse.noCoverageAnnotationType")) {
+            model.removeAnnotation(annotation);
+          }
+        }
+      } catch (Exception e) {
+      }
     }
   }
 }
