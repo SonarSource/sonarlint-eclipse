67473c28fd99fa8325a79c6b293a8f70ef21b8f8
jerr <l.jeremie@gmail.com>
2010-08-28 09:21:09 +0000

    SONARIDE-119: using images for "Rule priority" in violations view.
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@594 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/views/ViolationRulePriorityField.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/views/ViolationRulePriorityField.java
@@ -17,17 +17,72 @@
  */
 package org.sonar.ide.eclipse.views;
 
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.views.markers.MarkerField;
 import org.eclipse.ui.views.markers.MarkerItem;
+import org.sonar.ide.eclipse.SonarPlugin;
+import org.sonar.ide.shared.violations.ViolationUtils;
 
 /**
  * @author Jérémie Lagarde
  */
 public class ViolationRulePriorityField extends MarkerField {
 
+  private static Image[] images = null;
+
   @Override
-  public String getValue(MarkerItem item) {
-    return item.getMarker().getAttribute("rulepriority", "");
+  public int compare(MarkerItem item1, MarkerItem item2) {
+    return -(ViolationUtils.convertPriority(item1.getMarker().getAttribute("rulepriority", ""))-
+        ViolationUtils.convertPriority(item2.getMarker().getAttribute("rulepriority", "")));
   }
 
+  @Override
+  public String getColumnHeaderText() {
+    return "";
+  }
+
+  @Override
+  public String getColumnTooltipText() {
+    return "Priority";
+  }
+
+  @Override
+  public int getDefaultColumnWidth(Control control) {
+    return getPriorityImage("info").getBounds().width;
+  }
+
+  private Image getPriorityImage(MarkerItem item) {
+    return getPriorityImage(item.getMarker().getAttribute("rulepriority", ""));
+  }
+
+  private Image getPriorityImage(String priority) {
+    if(images == null) {
+      images = new Image[5];
+      images[0] = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/priority/blocker.gif").createImage(); //$NON-NLS-1$
+      images[1] = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/priority/critical.gif").createImage(); //$NON-NLS-1$
+      images[2] = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/priority/major.gif").createImage(); //$NON-NLS-1$
+      images[3] = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/priority/minor.gif").createImage(); //$NON-NLS-1$
+      images[4] = ImageDescriptor.createFromFile(SonarPlugin.class, "/org/sonar/ide/images/priority/info.gif").createImage(); //$NON-NLS-1$
+    }
+    return images[ViolationUtils.convertPriority(priority)];
+  }
+
+  @Override
+  public String getValue(MarkerItem item) {
+    return "";
+  }
+
+  @Override
+  public void update(ViewerCell cell) {
+    super.update(cell);
+    try {
+      Image image = getPriorityImage((MarkerItem) cell.getElement());
+      cell.setImage(image);
+    } catch (NumberFormatException e) {
+      return;
+    }
+  }
 }
