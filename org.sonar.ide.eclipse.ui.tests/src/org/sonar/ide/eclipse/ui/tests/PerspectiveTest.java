a6f329fcca6aaa07449d3f9a0e812855112f8b7f
Evgeny Mandrikov <mandrikov@gmail.com>
2010-09-03 17:38:31 +0000

    Fix integration test
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@595 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse.ui.tests/src/org/sonar/ide/eclipse/ui/tests/PerspectiveTest.java
+++ org.sonar.ide.eclipse.ui.tests/src/org/sonar/ide/eclipse/ui/tests/PerspectiveTest.java
@@ -19,11 +19,11 @@
 package org.sonar.ide.eclipse.ui.tests;
 
 import org.eclipse.jdt.ui.JavaUI;
-import org.eclipse.ui.IPageLayout;
 import org.junit.Test;
 import org.sonar.ide.eclipse.SonarUI;
 import org.sonar.ide.eclipse.views.MeasuresView;
 import org.sonar.ide.eclipse.views.NavigatorView;
+import org.sonar.ide.eclipse.views.ViolationsView;
 
 /**
  * @author Evgeny Mandrikov
@@ -33,13 +33,13 @@ public class PerspectiveTest extends UITestCase {
   @Test
   public void allViewsArePresent() {
     openPerspective(SonarUI.ID_PERSPECTIVE);
-    
+
     bot.viewById(JavaUI.ID_PACKAGES);
-    
+
     bot.viewById(MeasuresView.ID);
     bot.viewById(NavigatorView.ID);
 
-    bot.viewById(IPageLayout.ID_PROBLEM_VIEW);
+    bot.viewById(ViolationsView.ID);
   }
 
 }
