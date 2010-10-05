6b9d0945e300171672ccb2cb8b9b98a04bc0a75a
Evgeny Mandrikov <mandrikov@gmail.com>
2010-10-05 17:37:39 +0000

    Tune perspective
    git-svn-id: https://svn.codehaus.org/sonar-ide/trunk/eclipse@663 99cbfe3a-a47d-0410-a3ab-d8de1ee668d8

--- org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/perspectives/SonarPerspectiveFactory.java
+++ org.sonar.ide.eclipse/src/org/sonar/ide/eclipse/perspectives/SonarPerspectiveFactory.java
@@ -24,70 +24,59 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
 import org.sonar.ide.eclipse.views.HotspotsView;
 import org.sonar.ide.eclipse.views.MeasuresView;
 import org.sonar.ide.eclipse.views.RemoteView;
 import org.sonar.ide.eclipse.views.ViolationsView;
 
-/**
- * @author Jérémie Lagarde
- */
 public class SonarPerspectiveFactory implements IPerspectiveFactory {
 
-  private IPageLayout layout;
-
   public SonarPerspectiveFactory() {
-    super();
   }
 
   public void createInitialLayout(IPageLayout layout) {
-    this.layout = layout;
-    addViews();
-    addActionSets();
-    addNewWizardShortcuts();
-    addPerspectiveShortcuts();
-    addViewShortcuts();
-  }
+    String editorArea = layout.getEditorArea();
 
-  private void addViews() {
-    IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f, layout.getEditorArea()); //$NON-NLS-1$
+    IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f, editorArea); // $NON-NLS-1$
     left.addView("org.eclipse.jdt.ui.PackageExplorer");
+    left.addPlaceholder(IPageLayout.ID_PROJECT_EXPLORER);
 
-    IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.75f, layout.getEditorArea()); //$NON-NLS-1$
-    right.addView(MeasuresView.ID);
-
-    IFolderLayout bottom = layout.createFolder("bottomRight", IPageLayout.BOTTOM, 0.75f, layout.getEditorArea()); // NON-NLS-1$
+    IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea); // $NON-NLS-1$
     bottom.addView(RemoteView.ID);
     bottom.addView(HotspotsView.ID);
     bottom.addView(ViolationsView.ID);
-    bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
-  }
+    bottom.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+    bottom.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
 
-  private void addActionSets() {
-    layout.addActionSet("org.eclipse.debug.ui.launchActionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.debug.ui.debugActionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.debug.ui.profileActionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.jdt.debug.ui.JDTDebugActionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.jdt.junit.JUnitActionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.team.ui.actionSet"); // NON-NLS-1
-    layout.addActionSet("org.eclipse.jdt.ui.JavaActionSet");
-    layout.addActionSet("org.eclipse.jdt.ui.JavaElementCreationActionSet");
-    layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET); // NON-NLS-1
-  }
+    IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.75f, editorArea); //$NON-NLS-1$
+    right.addView(MeasuresView.ID);
 
-  private void addPerspectiveShortcuts() {
-    layout.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective"); // NON-NLS-1
-  }
+    // action sets
+    layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
 
-  private void addNewWizardShortcuts() {
-    layout.addNewWizardShortcut("org.sonar.ide.eclipse.wizards.newserverlocationwizard");// NON-NLS-1
-  }
+    // views - Sonar
+    layout.addShowViewShortcut(RemoteView.ID);
+    layout.addShowViewShortcut(HotspotsView.ID);
+    layout.addShowViewShortcut(MeasuresView.ID);
+    layout.addShowViewShortcut(ViolationsView.ID);
 
-  private void addViewShortcuts() {
+    // views - java
+    layout.addShowViewShortcut("org.eclipse.jdt.ui.PackageExplorer"); // $NON-NLS-1$
+
+    // views - debugging
     layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-    layout.addShowViewShortcut("org.eclipse.jdt.ui.PackageExplorer");
-    layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+    layout.addShowViewShortcut("org.eclipse.pde.runtime.LogView"); //$NON-NLS-1$
+
+    // views - standard workbench
     layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+    layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+    layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+    layout.addShowViewShortcut(IProgressConstants.PROGRESS_VIEW_ID);
+    layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
+
+    // new actions
+    layout.addNewWizardShortcut("org.sonar.ide.eclipse.wizards.newserverlocationwizard"); //$NON-NLS-1$
   }
 
 }
