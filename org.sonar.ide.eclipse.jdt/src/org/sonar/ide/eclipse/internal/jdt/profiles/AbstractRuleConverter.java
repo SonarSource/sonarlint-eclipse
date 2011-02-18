40136a98234c7917fe41ea06d8d87410f9e80686
Jérémie Lagarde <jer@printstacktrace.org>
2011-02-18 01:22:13 +0100

    SONARIDE-188 : Adding profile synchronisation preference

---
+++ org.sonar.ide.eclipse.jdt/src/org/sonar/ide/eclipse/internal/jdt/profiles/AbstractRuleConverter.java
@@ -0,0 +1,69 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2010 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.ide.eclipse.internal.jdt.profiles;
+
+import org.apache.commons.lang.StringUtils;
+import org.sonar.ide.eclipse.jdt.profiles.ISonarRuleConverter;
+import org.sonar.wsclient.services.Rule;
+import org.sonar.wsclient.services.RuleParam;
+
+/**
+ * @author Jérémie Lagarde
+ * @since 1.1.0
+ */
+public abstract class AbstractRuleConverter implements ISonarRuleConverter {
+
+  private String plugin;
+  private String key;
+
+  public AbstractRuleConverter(String plugin, String key) {
+    this.plugin = plugin;
+    this.key = key;
+  }
+
+  public final boolean canConvert(Rule rule) {
+    return (rule != null && rule.isActive() && plugin.equalsIgnoreCase(rule.getRepository()) && key.equalsIgnoreCase(rule.getKey()));
+  }
+  
+  protected String getParam(Rule rule, String name) {
+    return getParam(rule, name, null);
+  }
+
+  protected String getParam(Rule rule, String name, String defaultValue) {
+    for (RuleParam param : rule.getParams()) {
+      if (param.getName().equals(name))
+        return (StringUtils.isBlank(param.getValue())) ? defaultValue : param.getValue();
+    }
+    return null;
+  }
+  protected String[] getParams(Rule rule, String name) {
+    return getParams(rule, name, null);
+  }
+
+  protected String[] getParams(Rule rule, String name, String[] defaultValues) {
+    for (RuleParam param : rule.getParams()) {
+      if (param.getName().equals(name))
+        return (StringUtils.isBlank(param.getValue())) ? defaultValues : StringUtils.split(param.getValue());
+    }
+    return null;
+  }
+
+}
---
+++ org.sonar.ide.eclipse.jdt/src/org/sonar/ide/eclipse/internal/jdt/profiles/checkstyle/LineLengthCheckstyleConverter.java
@@ -0,0 +1,55 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2010 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.ide.eclipse.internal.jdt.profiles.checkstyle;
+
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
+import org.sonar.ide.eclipse.internal.jdt.profiles.ProfileConfiguration;
+import org.sonar.wsclient.services.Rule;
+
+/**
+ * @author Jérémie Lagarde
+ * @since 1.1.0
+ * 
+ * Class to convert Checkstyle LineLengthCheck rule to eclipse profile. 
+ * 
+ *        ex : {"title":"Line Length", "key":"checkstyle:com.puppycrawl.tools.checkstyle.checks.sizes.LineLengthCheck",
+ *        "plugin":"checkstyle", "description":"Checks for long lines.","priority":"MAJOR","status":"ACTIVE", "params":[
+ *        {"name":"ignorePattern","description":"pattern for lines to ignore"},
+ *        {"name":"max","description":"maximum allowable line length. Default is 80.","value":"180"},
+ *        {"name":"tabWidth","description":"number of expanded spaces for a tab character. Default is 8.","value":"4"}]},
+ */
+public class LineLengthCheckstyleConverter extends AbstractCheckstyleConverter {
+
+  protected final static String KEY = "checkstyle:com.puppycrawl.tools.checkstyle.checks.sizes.LineLengthCheck";
+
+  public LineLengthCheckstyleConverter() {
+    super(KEY);
+  }
+
+  public void convert(ProfileConfiguration config, Rule rule) {
+    String max = getParam(rule, "max", "80");
+    String tabWith = getParam(rule, "tabWith", "8");
+    config.add(DefaultCodeFormatterConstants.FORMATTER_LINE_SPLIT, max);
+    config.add(DefaultCodeFormatterConstants.FORMATTER_COMMENT_LINE_LENGTH, max);
+    config.add(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE, tabWith);
+  }
+
+}
