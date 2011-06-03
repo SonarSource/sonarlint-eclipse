c97c2e33cd715335574ffc310f5b8ef5947a6ec8
Evgeny Mandrikov <mandrikov@gmail.com>
2011-06-03 12:25:06 +0400

    Update target platform

--- target-platform/update.sh
+++ target-platform/update.sh
@@ -8,6 +8,6 @@ fi
 echo Updating $TARGET
 
 # Use the following command to update .target file to match contents of remote p2 repositories.
-mvn org.sonatype.tycho.extras:tycho-version-bump-plugin:0.11.0-SNAPSHOT:update-target \
- -Dtycho.mode=maven -Dtycho.version=0.11.1 \
+mvn org.sonatype.tycho.extras:tycho-version-bump-plugin:0.12.0-SNAPSHOT:update-target \
+ -Dtycho.mode=maven -Dtycho.version=0.12.0 \
  -Dtarget=$TARGET
