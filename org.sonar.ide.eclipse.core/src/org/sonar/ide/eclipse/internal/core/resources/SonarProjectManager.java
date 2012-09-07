5e3b5f8eb2dfd3f533902e7f72e80376f4f7a62a
David Gageot <david@gageot.net>
2012-09-07 14:11:59 +0200

    Clean up code and increase log level

--- org.sonar.ide.eclipse.core/src/org/sonar/ide/eclipse/internal/core/resources/SonarProjectManager.java
+++ org.sonar.ide.eclipse.core/src/org/sonar/ide/eclipse/internal/core/resources/SonarProjectManager.java
@@ -33,9 +33,9 @@ import org.sonar.ide.eclipse.core.SonarCorePlugin;
  * @author Evgeny Mandrikov
  */
 public class SonarProjectManager {
-
   private static final Logger LOG = LoggerFactory.getLogger(SonarProjectManager.class);
 
+  private static final String VERSION = "1";
   private static final String P_VERSION = "version";
   private static final String P_SONAR_SERVER_URL = "serverUrl";
   private static final String P_PROJECT_GROUPID = "projectGroupId";
@@ -43,16 +43,16 @@ public class SonarProjectManager {
   private static final String P_PROJECT_BRANCH = "projectBranch";
   private static final String P_ANALYSE_LOCALLY = "analyseLocally";
 
-  private static final String VERSION = "1";
-
   public ProjectProperties readSonarConfiguration(IProject project) {
     LOG.debug("Reading configuration for project " + project.getName());
+
     IScopeContext projectScope = new ProjectScope(project);
     IEclipsePreferences projectNode = projectScope.getNode(SonarCorePlugin.PLUGIN_ID);
     if (projectNode == null) {
-      LOG.warn("Unable to read configuration");
+      LOG.error("Unable to read configuration for project " + project.getName());
       return new ProjectProperties(project);
     }
+
     String version = projectNode.get(P_VERSION, null);
     // Godin: we can perform migration here
     String artifactId = projectNode.get(P_PROJECT_ARTIFACTID, "");
@@ -62,13 +62,13 @@ public class SonarProjectManager {
       }
     }
 
-    ProjectProperties configuration = new ProjectProperties(project);
-    configuration.setUrl(projectNode.get(P_SONAR_SERVER_URL, ""));
-    configuration.setGroupId(projectNode.get(P_PROJECT_GROUPID, ""));
-    configuration.setArtifactId(artifactId);
-    configuration.setBranch(projectNode.get(P_PROJECT_BRANCH, ""));
-    configuration.setAnalysedLocally(projectNode.getBoolean(P_ANALYSE_LOCALLY, false));
-    return configuration;
+    ProjectProperties properties = new ProjectProperties(project);
+    properties.setUrl(projectNode.get(P_SONAR_SERVER_URL, ""));
+    properties.setGroupId(projectNode.get(P_PROJECT_GROUPID, ""));
+    properties.setArtifactId(artifactId);
+    properties.setBranch(projectNode.get(P_PROJECT_BRANCH, ""));
+    properties.setAnalysedLocally(projectNode.getBoolean(P_ANALYSE_LOCALLY, false));
+    return properties;
   }
 
   /**
@@ -77,23 +77,25 @@ public class SonarProjectManager {
   public boolean saveSonarConfiguration(IProject project, ProjectProperties configuration) {
     IScopeContext projectScope = new ProjectScope(project);
     IEclipsePreferences projectNode = projectScope.getNode(SonarCorePlugin.PLUGIN_ID);
-    if (projectNode != null) {
-      LOG.debug("Saving configuration for project " + project.getName());
-      projectNode.put(P_VERSION, VERSION);
-
-      projectNode.put(P_SONAR_SERVER_URL, configuration.getUrl());
-      projectNode.put(P_PROJECT_GROUPID, configuration.getGroupId());
-      projectNode.put(P_PROJECT_ARTIFACTID, configuration.getArtifactId());
-      projectNode.put(P_PROJECT_BRANCH, configuration.getBranch());
-      projectNode.putBoolean(P_ANALYSE_LOCALLY, configuration.isAnalysedLocally());
-
-      try {
-        projectNode.flush();
-        return true;
-      } catch (BackingStoreException e) {
-        LOG.error("Failed to save project configuration", e);
-      }
+    if (projectNode == null) {
+      return false;
     }
-    return false;
+
+    LOG.debug("Saving configuration for project " + project.getName());
+    projectNode.put(P_VERSION, VERSION);
+
+    projectNode.put(P_SONAR_SERVER_URL, configuration.getUrl());
+    projectNode.put(P_PROJECT_GROUPID, configuration.getGroupId());
+    projectNode.put(P_PROJECT_ARTIFACTID, configuration.getArtifactId());
+    projectNode.put(P_PROJECT_BRANCH, configuration.getBranch());
+    projectNode.putBoolean(P_ANALYSE_LOCALLY, configuration.isAnalysedLocally());
+
+    try {
+      projectNode.flush();
+      return true;
+    } catch (BackingStoreException e) {
+      LOG.error("Failed to save project configuration", e);
+      return false;
+    }
   }
 }
