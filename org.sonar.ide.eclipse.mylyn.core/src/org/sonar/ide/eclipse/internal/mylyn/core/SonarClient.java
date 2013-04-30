7a3b775a0bfec7f62b29c04c4eea33930ac58384
Julien HENRY <julien.henry@sonarsource.com>
2013-04-30 11:02:17 +0200

    Prepare next sprint: * Updated parent POM and Tycho version * Updated license headers

--- org.sonar.ide.eclipse.mylyn.core/src/org/sonar/ide/eclipse/internal/mylyn/core/SonarClient.java
+++ org.sonar.ide.eclipse.mylyn.core/src/org/sonar/ide/eclipse/internal/mylyn/core/SonarClient.java
@@ -1,6 +1,6 @@
 /*
  * Sonar Eclipse
- * Copyright (C) 2010-2012 SonarSource
+ * Copyright (C) 2010-2013 SonarSource
  * dev@sonar.codehaus.org
  *
  * This program is free software; you can redistribute it and/or
