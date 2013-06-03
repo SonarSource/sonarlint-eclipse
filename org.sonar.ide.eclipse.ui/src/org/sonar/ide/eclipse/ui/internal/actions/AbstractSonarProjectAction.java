a4d7c115ebf0c81fdb5fd3f96dcb66a7162df8c2
Julien HENRY <julien.henry@sonarsource.com>
2013-06-03 14:57:15 +0200

    Fix some quality flaws

---
+++ org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/actions/AbstractSonarProjectAction.java
@@ -0,0 +1,114 @@
+/*
+ * Sonar Eclipse
+ * Copyright (C) 2010-2013 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.ide.eclipse.ui.internal.actions;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.ide.eclipse.core.internal.resources.SonarProject;
+import org.sonar.ide.eclipse.ui.internal.views.issues.IssuesView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AbstractSonarProjectAction implements IObjectActionDelegate {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AbstractSonarProjectAction.class);
+
+  private List<IProject> selectedProjects = new ArrayList<IProject>();
+
+  public AbstractSonarProjectAction() {
+    super();
+  }
+
+  protected List<IProject> getSelectedProjects() {
+    return selectedProjects;
+  }
+
+  /**
+   * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+   */
+  public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+  }
+
+  protected void showIssuesViewAfterJobSuccess(Job job) {
+    // Display issues view after analysis is completed
+    job.addJobChangeListener(new JobChangeAdapter() {
+      @Override
+      public void done(IJobChangeEvent event) {
+        if (Status.OK_STATUS == event.getResult()) {
+          Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+              IWorkbenchWindow iw = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+              try {
+                iw.getActivePage().showView(IssuesView.ID);
+              } catch (PartInitException e) {
+                LOG.error("Unable to open Issues View", e);
+              }
+            }
+          });
+        }
+      }
+    });
+    job.schedule();
+  }
+
+  /**
+   * @see org.eclipse.ui.IActionDelegate#selectionChanged(IAction, ISelection)
+   */
+  public void selectionChanged(IAction action, ISelection selection) {
+    selectedProjects.clear();
+    boolean actionEnabled = true;
+    // get All selected Elements
+    if (selection instanceof IStructuredSelection) {
+      IStructuredSelection strucSelection = (IStructuredSelection) selection;
+      for (Iterator<Object> iterator = strucSelection.iterator(); iterator.hasNext();) {
+        Object element = iterator.next();
+
+        IProject project = (IProject) element;
+        SonarProject projectProperties = SonarProject.getInstance(project);
+        actionEnabled &= actionEnabled(projectProperties);
+        selectedProjects.add(project);
+      }
+      action.setEnabled(actionEnabled);
+    }
+    else {
+      action.setEnabled(false);
+    }
+
+  }
+
+  protected abstract boolean actionEnabled(SonarProject projectProperties);
+
+}
--- org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/actions/AnalyseProjectAction.java
+++ org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/actions/AnalyseProjectAction.java
@@ -19,44 +19,17 @@
  */
 package org.sonar.ide.eclipse.ui.internal.actions;
 
-import org.sonar.ide.eclipse.ui.internal.views.issues.IssuesView;
-
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.sonar.ide.eclipse.core.internal.jobs.AnalyseProjectJob;
 import org.sonar.ide.eclipse.core.internal.resources.SonarProject;
 import org.sonar.ide.eclipse.ui.internal.SonarUiPlugin;
 import org.sonar.ide.eclipse.ui.internal.console.SonarConsole;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-public class AnalyseProjectAction implements IObjectActionDelegate {
-
-  private static final Logger LOG = LoggerFactory.getLogger(AnalyseProjectAction.class);
+public class AnalyseProjectAction extends AbstractSonarProjectAction {
 
   public AnalyseProjectAction() {
     super();
-  }
-
-  /**
-   * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
-   */
-  public void setActivePart(IAction action, IWorkbenchPart targetPart) {
   }
 
   /**
@@ -65,56 +38,16 @@ public class AnalyseProjectAction implements IObjectActionDelegate {
   public void run(IAction action) {
     boolean debugEnabled = SonarConsole.isDebugEnabled();
     SonarUiPlugin.getDefault().getSonarConsole().clearConsole();
-    for (IProject project : projects) {
+    for (IProject project : getSelectedProjects()) {
       AnalyseProjectJob job = new AnalyseProjectJob(project, debugEnabled,
           SonarUiPlugin.getExtraPropertiesForLocalAnalysis(project), SonarUiPlugin.getSonarJvmArgs());
-      // Display issues view after analysis is completed
-      job.addJobChangeListener(new JobChangeAdapter() {
-        @Override
-        public void done(IJobChangeEvent event) {
-          if (Status.OK_STATUS == event.getResult()) {
-            Display.getDefault().asyncExec(new Runnable() {
-              public void run() {
-                IWorkbenchWindow iw = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-                try {
-                  iw.getActivePage().showView(IssuesView.ID);
-                } catch (PartInitException e) {
-                  LOG.error("Unable to open Issues View", e);
-                }
-              }
-            });
-          }
-        }
-      });
-      job.schedule();
+      showIssuesViewAfterJobSuccess(job);
     }
   }
 
-  private List<IProject> projects = new ArrayList<IProject>();
-
-  /**
-   * @see org.eclipse.ui.IActionDelegate#selectionChanged(IAction, ISelection)
-   */
-  public void selectionChanged(IAction action, ISelection selection) {
-    projects.clear();
-    boolean actionEnabled = true;
-    // get All selected Elements
-    if (selection instanceof IStructuredSelection) {
-      IStructuredSelection strucSelection = (IStructuredSelection) selection;
-      for (Iterator<Object> iterator = strucSelection.iterator(); iterator.hasNext();) {
-        Object element = iterator.next();
-
-        IProject project = (IProject) element;
-        SonarProject projectProperties = SonarProject.getInstance(project);
-        actionEnabled &= projectProperties.isAnalysedLocally();
-        projects.add(project);
-      }
-      action.setEnabled(actionEnabled);
-    }
-    else {
-      action.setEnabled(false);
-    }
-
+  @Override
+  protected boolean actionEnabled(SonarProject projectProperties) {
+    return projectProperties.isAnalysedLocally();
   }
 
 }
