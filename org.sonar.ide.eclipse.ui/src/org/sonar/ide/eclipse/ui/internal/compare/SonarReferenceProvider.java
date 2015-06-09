d84fab52bc9230b36fa6d3d4224d4f14f6b08323
Julien HENRY <julien.henry@sonarsource.com>
2015-06-09 17:46:24 +0200

    Update parent pom and add target platform for Luna and Mars

--- org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/compare/SonarReferenceProvider.java
+++ org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/compare/SonarReferenceProvider.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Eclipse
- * Copyright (C) 2010-2014 SonarSource
+ * Copyright (C) 2010-2015 SonarSource
  * dev@sonar.codehaus.org
  *
  * This program is free software; you can redistribute it and/or
