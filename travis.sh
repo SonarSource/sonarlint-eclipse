c1a17ba34efbe8d9b494dc3eefdbfd538e942288
Julien HENRY <julien.henry@sonarsource.com>
2015-07-01 11:01:29 +0200

    Try to fix IT execution

--- travis.sh
+++ travis.sh
@@ -3,7 +3,19 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v2.1/install.sh | bash
+  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/master/install.sh | bash
+}
+
+function start_xvfb {
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+}
+
+function install_jars {
+  echo "Install jars into local maven repository"
+
+  mkdir -p ~/.m2/repository
+  cp -r /tmp/travis-utils/m2repo/* ~/.m2/repository
 }
 
 installTravisTools
@@ -13,6 +25,7 @@ if [ "${TARGET_PLATFORM}" == "e44" ] || [ "${TARGET_PLATFORM}" == "e45" ]
 then
   cd integrationTests
   start_xvfb
+  install_jars
   mvn clean verify -Dsonar-eclipse.p2.url=file:///home/travis/build/SonarSource/sonar-eclipse/org.sonar.ide.eclipse.site/target/repository/ -Dsonar.runtimeVersion=DEV -DjavaVersion=LATEST_RELEASE -DcppVersion=LATEST_RELEASE -DpythonVersion=LATEST_RELEASE
 fi  
 
