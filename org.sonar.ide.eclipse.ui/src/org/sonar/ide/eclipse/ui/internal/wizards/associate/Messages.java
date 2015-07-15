3ae852c62bf071114cafd35b1060ea7656a17d90
Hemantkumar Chigadani <hemanthic@gmail.com>
2015-07-15 11:55:07 +0100

    Copy-right update

--- org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/wizards/associate/Messages.java
+++ org.sonar.ide.eclipse.ui/src/org/sonar/ide/eclipse/ui/internal/wizards/associate/Messages.java
@@ -1,15 +1,30 @@
 /*
- * Copyright © 2015 Siemens Medical Solutions USA, Inc.
- * All Rights Reserved.
+ * SonarQube Eclipse
+ * Copyright (C) 2010-2015 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
 package org.sonar.ide.eclipse.ui.internal.wizards.associate;
 
 import org.eclipse.osgi.util.NLS;
 
 /**
- * TODO Insert description sentence here.
+ * Messsages.
  *
- * @author z0035kms/Hemantkumar Chigadani
+ * @author Hemantkumar Chigadani
  */
 public class Messages extends NLS {
   private static final String BUNDLE_NAME = "org.sonar.ide.eclipse.ui.internal.wizards.associate.messages"; //$NON-NLS-1$
