ae24cad893f29d228a07bbc2d0320a5f5773f28c
Julien HENRY <julien.henry@sonarsource.com>
2015-08-03 16:45:42 +0200

    Update ITs to run with SQ LTS on one axis

--- travis.sh
+++ travis.sh
@@ -20,5 +20,5 @@ then
   metacity --sm-disable --replace &
 
   cd integrationTests
-  mvn verify -Dsonar-eclipse.p2.url=file:///home/travis/build/SonarSource/sonar-eclipse/org.sonar.ide.eclipse.site/target/repository/ -Dsonar.runtimeVersion=DEV -DjavaVersion=LATEST_RELEASE -DpythonVersion=LATEST_RELEASE
+  mvn verify -Dsonar-eclipse.p2.url=file:///home/travis/build/SonarSource/sonar-eclipse/org.sonar.ide.eclipse.site/target/repository/ -Dsonar.runtimeVersion=$SQ_VERSION -DjavaVersion=LATEST_RELEASE -DpythonVersion=LATEST_RELEASE
 fi
