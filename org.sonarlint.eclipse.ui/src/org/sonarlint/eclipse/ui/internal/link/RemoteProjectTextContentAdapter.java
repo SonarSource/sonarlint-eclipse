321d6ace87156de5bb131abb15204bf6708a0cc9
Julien HENRY <julien.henry@sonarsource.com>
2016-03-07 17:55:40 +0100

    Update version to 2.0.0

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/link/RemoteProjectTextContentAdapter.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/link/RemoteProjectTextContentAdapter.java
@@ -1,7 +1,7 @@
 /*
- * SonarQube Eclipse
- * Copyright (C) 2010-2015 SonarSource
- * sonarqube@googlegroups.com
+ * SonarLint for Eclipse
+ * Copyright (C) 2015-2016 SonarSource SA
+ * sonarlint@sonarsource.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,9 +13,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.sonarlint.eclipse.ui.internal.link;
 
