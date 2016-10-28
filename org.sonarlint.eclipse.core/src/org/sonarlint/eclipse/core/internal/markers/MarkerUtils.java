28289707f578025129a6a9148fa55acbebf6114d
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-28 16:20:33 +0200

    Drop null check for textRange that is never null

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/markers/MarkerUtils.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/markers/MarkerUtils.java
@@ -75,7 +75,7 @@ public final class MarkerUtils {
 
   @CheckForNull
   public static FlatTextRange getFlatTextRange(final IDocument document, TextRange textRange) {
-    if (textRange == null || textRange.getStartLine() == null) {
+    if (textRange.getStartLine() == null) {
       return null;
     }
     if (textRange.getStartLineOffset() == null) {
