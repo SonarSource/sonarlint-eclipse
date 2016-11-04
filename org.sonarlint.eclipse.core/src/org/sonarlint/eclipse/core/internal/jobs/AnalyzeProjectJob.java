0085efb59f3da50066a0e854f7515876ad56512b
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-11-04 16:49:41 +0100

    Use protobuf embeded in SL-CORE and other small fixes

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/jobs/AnalyzeProjectJob.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/jobs/AnalyzeProjectJob.java
@@ -19,12 +19,7 @@
  */
 package org.sonarlint.eclipse.core.internal.jobs;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -155,72 +150,9 @@ public class AnalyzeProjectJob extends AbstractSonarProjectJob {
     }
   }
 
-  private final class EclipseInputFile implements ClientInputFile {
-    private final List<PathMatcher> pathMatchersForTests;
-    private final IFile file;
-    private final Path filePath;
-
-    private EclipseInputFile(List<PathMatcher> pathMatchersForTests, IFile file, Path filePath) {
-      this.pathMatchersForTests = pathMatchersForTests;
-      this.file = file;
-      this.filePath = filePath;
-    }
-
-    @Override
-    public String getPath() {
-      return filePath.toString();
-    }
-
-    @Override
-    public boolean isTest() {
-      for (PathMatcher matcher : pathMatchersForTests) {
-        if (matcher.matches(filePath)) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    @Override
-    public Charset getCharset() {
-      try {
-        return Charset.forName(file.getCharset());
-      } catch (CoreException e) {
-        return null;
-      }
-    }
-
-    @Override
-    public <G> G getClientObject() {
-      return (G) file;
-    }
-
-    @Override
-    public String contents() throws IOException {
-      try (TextFileContext context = new TextFileContext(file)) {
-        return context.getDocument().get();
-      } catch (CoreException e) {
-        throw new IOException("error while reading file: " + file.getFullPath(), e);
-      }
-    }
-
-    @Override
-    public InputStream inputStream() throws IOException {
-      Charset charset = getCharset();
-      if (charset == null) {
-        charset = StandardCharsets.UTF_8;
-      }
-      try (TextFileContext context = new TextFileContext(file)) {
-        return new ByteArrayInputStream(contents().getBytes(charset));
-      } catch (CoreException e) {
-        throw new IOException("error while streaming file: " + file.getFullPath(), e);
-      }
-    }
-  }
-
   @Override
   protected IStatus doRun(final IProgressMonitor monitor) {
-
+    long startTime = System.currentTimeMillis();
     // Analyze
     try {
       // Configure
@@ -242,6 +174,7 @@ public class AnalyzeProjectJob extends AbstractSonarProjectJob {
       }
 
       analysisCompleted(usedConfigurators, mergedExtraProps, monitor);
+      SonarLintCorePlugin.getDefault().debug(String.format("Analysis completed in %d ms", System.currentTimeMillis() - startTime));
     } catch (Exception e) {
       SonarLintCorePlugin.getDefault().error("Error during execution of SonarLint analysis", e);
       return new Status(Status.WARNING, SonarLintCorePlugin.PLUGIN_ID, "Error when executing SonarLint analysis", e);
