b1d579befcb783f30c5ad626119cc068cf143aa9
Julien HENRY <julien.henry@sonarsource.com>
2017-04-27 15:05:13 +0200

    SLE-173 Rename page About -> Miscellaneous

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
@@ -43,7 +43,7 @@ public class AboutPropertyPage extends PropertyPage implements IWorkbenchPrefere
   private Button enabledBtn;
 
   public AboutPropertyPage() {
-    setTitle("About");
+    setTitle("Miscellaneous");
   }
 
   @Override
