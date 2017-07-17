4a00db58d69056e961d39da3f97501f5cae16953
Julien HENRY <julien.henry@sonarsource.com>
2017-07-17 15:53:21 +0200

    Fix ITs

--- its/org.sonarlint.eclipse.its/src/org/sonarlint/eclipse/its/ConnectedModeTest.java
+++ its/org.sonarlint.eclipse.its/src/org/sonarlint/eclipse/its/ConnectedModeTest.java
@@ -41,6 +41,9 @@ public class ConnectedModeTest extends AbstractSonarLintTest {
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
+    // Need at least one plugin to avoid bug SONAR-8918
+    .setOrchestratorProperty("javaVersion", "LATEST_RELEASE")
+    .addPlugin("java")
     .build();
 
   private static WsClient adminWsClient;
