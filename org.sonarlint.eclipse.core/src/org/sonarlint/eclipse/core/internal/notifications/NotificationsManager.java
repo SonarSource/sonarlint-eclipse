8d26490932da18d62d2cebd6a461d8415b210e66
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-07-21 11:54:41 +0200

    Move main notifications code to core

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/notifications/NotificationsManager.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/notifications/NotificationsManager.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarlint.eclipse.ui.internal.notifications;
+package org.sonarlint.eclipse.core.internal.notifications;
 
 import java.time.ZonedDateTime;
 import java.util.HashMap;
