6475bb956a47a541554f3a6c259680d4d4b87616
Duarte Meneses <duarte.meneses@sonarsource.com>
2018-02-23 10:10:42 +0100

    Disable some ITs e44

--- its/org.sonarlint.eclipse.its/src/org/sonarlint/eclipse/its/MavenTest.java
+++ its/org.sonarlint.eclipse.its/src/org/sonarlint/eclipse/its/MavenTest.java
@@ -60,13 +60,15 @@ public class MavenTest extends AbstractSonarLintTest {
       .extracting(markerAttributes(IMarker.LINE_NUMBER, IMarker.MESSAGE)).containsOnly(
         tuple("/sample-module1/src/main/java/hello/Hello1.java", 9, "Replace this use of System.out or System.err by a logger."));
 
-    // Issues on pom.xml
-    // new JavaPackageExplorerBot(bot)
-    // .expandAndDoubleClick("sample-maven", "pom.xml");
-    // JobHelpers.waitForJobsToComplete(bot);
-    // assertThat(Arrays.asList(root.findMember("pom.xml").findMarkers(MARKER_ON_THE_FLY_ID, true, IResource.DEPTH_ONE)))
-    // .extracting(markerAttributes(IMarker.LINE_NUMBER, IMarker.MESSAGE)).containsOnly(
-    // tuple("/sample-maven/pom.xml", 11, "Replace \"pom.name\" with \"project.name\"."));
+    if (!platformVersion().toString().startsWith("4.4")) {
+      // Issues on pom.xml
+      new JavaPackageExplorerBot(bot)
+        .expandAndDoubleClick("sample-maven", "pom.xml");
+      JobHelpers.waitForJobsToComplete(bot);
+      assertThat(Arrays.asList(root.findMember("pom.xml").findMarkers(MARKER_ON_THE_FLY_ID, true, IResource.DEPTH_ONE)))
+        .extracting(markerAttributes(IMarker.LINE_NUMBER, IMarker.MESSAGE)).containsOnly(
+          tuple("/sample-maven/pom.xml", 11, "Replace \"pom.name\" with \"project.name\"."));
+    }
   }
 
 }
