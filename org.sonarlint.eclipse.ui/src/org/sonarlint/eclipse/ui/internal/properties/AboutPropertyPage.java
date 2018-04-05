9948a563726df60f34a0e2d34a049368089826c4
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-04-05 11:50:06 +0200

    Update telemetry sample data

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
@@ -77,6 +77,7 @@ public class AboutPropertyPage extends PropertyPage implements IWorkbenchPrefere
       + "    \"connected_mode_used\": true\n"
       + "    \"system_time\":\"2018-02-27T16:31:49.173+01:00\",\n"
       + "    \"install_time\":\"2018-02-01T16:30:49.124+01:00\"\n"
+      + "    \"analyses\":[{\"language\":\"java\",\"rate_per_duration\":{\"0-300\":100,\"300-500\":0,\"500-1000\":0,\"1000-2000\":0,\"2000-4000\":0,\"4000+\":0}}]\n"
       + "}");
 
     text.addSelectionListener(new SelectionAdapter() {
