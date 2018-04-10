f6032acc47aad2ba505acdda6839f9269a9707a6
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-04-10 19:53:01 +0200

    Hide deactivate rule context menu on markers on bound projects

---
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/resources/MarkerPropertyTester.java
@@ -0,0 +1,46 @@
+/*
+ * SonarLint for Eclipse
+ * Copyright (C) 2015-2018 SonarSource SA
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarlint.eclipse.core.internal.resources;
+
+import javax.annotation.Nullable;
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IMarker;
+import org.sonarlint.eclipse.core.internal.adapter.Adapters;
+import org.sonarlint.eclipse.core.resource.ISonarLintFile;
+
+public class MarkerPropertyTester extends PropertyTester {
+
+  /**
+   * Test if a marker corresponds to a standalone project.
+   */
+  @Override
+  public boolean test(Object receiver, String property, Object[] args, @Nullable Object expectedValue) {
+    IMarker marker = Adapters.adapt(receiver, IMarker.class);
+    if (marker == null) {
+      return false;
+    }
+
+    ISonarLintFile sonarLintFile = Adapters.adapt(marker.getResource(), ISonarLintFile.class);
+    if (sonarLintFile == null) {
+      return false;
+    }
+    return !sonarLintFile.getProject().isBound();
+  }
+}
