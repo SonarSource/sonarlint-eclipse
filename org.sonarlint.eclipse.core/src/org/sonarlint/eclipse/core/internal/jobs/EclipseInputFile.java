0b81cafca052567af2c74ad802d7d5302a29df0c
Julien HENRY <julien.henry@sonarsource.com>
2019-01-31 09:29:01 +0100

    SLE-301 Remove the debug stacktrace

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/jobs/EclipseInputFile.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/jobs/EclipseInputFile.java
@@ -32,7 +32,6 @@ import javax.annotation.Nullable;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.jface.text.IDocument;
-import org.sonarlint.eclipse.core.SonarLintLogger;
 import org.sonarlint.eclipse.core.resource.ISonarLintFile;
 import org.sonarsource.sonarlint.core.client.api.common.analysis.ClientInputFile;
 
@@ -67,7 +66,6 @@ class EclipseInputFile implements ClientInputFile {
   }
 
   private synchronized void initFromFS(ISonarLintFile file, Path temporaryDirectory) {
-    SonarLintLogger.get().debug("Forced to init a physical file", new Exception());
     IFileStore fileStore;
     try {
       fileStore = EFS.getStore(file.getResource().getLocationURI());
