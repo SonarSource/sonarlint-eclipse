941da04bbe612cc1024f7474a5def8078a1acada
Julien HENRY <julien.henry@sonarsource.com>
2021-02-18 14:57:26 +0100

    Initial table for the hotspot

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/hotspots/HotspotsView.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/hotspots/HotspotsView.java
@@ -19,16 +19,50 @@
  */
 package org.sonarlint.eclipse.ui.internal.hotspots;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.widgets.Form;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.part.PageBook;
 import org.eclipse.ui.part.ViewPart;
+import org.sonarlint.eclipse.ui.internal.SonarLintImages;
 import org.sonarlint.eclipse.ui.internal.SonarLintUiPlugin;
 import org.sonarlint.eclipse.ui.internal.flowlocations.SonarLintMarkerSelectionListener;
+import org.sonarsource.sonarlint.core.client.api.common.TextRange;
+import org.sonarsource.sonarlint.core.serverapi.hotspot.ServerHotspot;
+import org.sonarsource.sonarlint.core.serverapi.hotspot.ServerHotspot.Resolution;
+import org.sonarsource.sonarlint.core.serverapi.hotspot.ServerHotspot.Rule;
+import org.sonarsource.sonarlint.core.serverapi.hotspot.ServerHotspot.Rule.Probability;
+import org.sonarsource.sonarlint.core.serverapi.hotspot.ServerHotspot.Status;
 
 public class HotspotsView extends ViewPart implements SonarLintMarkerSelectionListener {
 
   public static final String ID = SonarLintUiPlugin.PLUGIN_ID + ".views.HotspotsView";
+  private PageBook book;
+  private Control hotspotsPage;
+  private TableViewer hotspotViewer;
+  private SashForm splitter;
 
   @Override
   public void markerSelected(Optional<IMarker> marker) {
@@ -37,10 +71,128 @@ public class HotspotsView extends ViewPart implements SonarLintMarkerSelectionLi
   @Override
   public void createPartControl(Composite parent) {
     SonarLintUiPlugin.getSonarlintMarkerSelectionService().addMarkerSelectionListener(this);
+
+    createToolbar();
+
+    FormToolkit toolkit = new FormToolkit(parent.getDisplay());
+    book = new PageBook(parent, SWT.NONE);
+
+    Control noHotspotsMessage = createNoHotspotsMessage(toolkit);
+    hotspotsPage = createHotspotsPage(toolkit);
+    book.showPage(noHotspotsMessage);
+
+  }
+
+  private void createToolbar() {
+    IToolBarManager toolbarManager = getViewSite().getActionBars().getToolBarManager();
+    OpenFakeHotspotAction action = new OpenFakeHotspotAction();
+    toolbarManager.add(action);
+    toolbarManager.add(new Separator());
+    toolbarManager.update(false);
+  }
+
+  private class OpenFakeHotspotAction extends Action {
+
+    /**
+     * Constructs a new action.
+     */
+    public OpenFakeHotspotAction() {
+      super("Open fake hotspot");
+      setDescription("Open fake hotspot");
+      setToolTipText("Open fake hotspot");
+      setImageDescriptor(SonarLintImages.DEBUG);
+    }
+
+    /**
+     * Runs the action.
+     */
+    @Override
+    public void run() {
+      ServerHotspot hotspot = new ServerHotspot("Some message", "foo/bar/Foo.java", new TextRange(1, 2, 3, 4), "henryju", Status.TO_REVIEW, Resolution.FIXED,
+        new Rule("java:S123", "Do not do this", "foo", Probability.LOW, "Risk", "Vulnerability", "Fix"));
+      openHotspot(hotspot);
+    }
+
+  }
+
+  private Control createNoHotspotsMessage(FormToolkit kit) {
+    Form form = kit.createForm(book);
+    Composite body = form.getBody();
+    GridLayout layout = new GridLayout();
+    body.setLayout(layout);
+
+    Link emptyMsg = new Link(body, SWT.CENTER | SWT.WRAP);
+    emptyMsg.setText("You can open a Security Hotspot from SonarQube. <a>More infos</a>");
+    GridData gd = new GridData(SWT.LEFT, SWT.FILL, true, false);
+    emptyMsg.setLayoutData(gd);
+    emptyMsg.setBackground(emptyMsg.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
+    emptyMsg.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        try {
+          PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser().openURL(new URL("https://github.com/SonarSource/sonarlint-eclipse/wiki/Security-Hotspots"));
+        } catch (PartInitException | MalformedURLException e1) {
+          // ignore
+        }
+      }
+    });
+    return form;
+  }
+
+  private Control createHotspotsPage(FormToolkit kit) {
+    Form form = kit.createForm(book);
+    Composite body = form.getBody();
+    GridLayout layout = new GridLayout();
+    body.setLayout(layout);
+
+    splitter = new SashForm(body, SWT.NONE);
+    GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+    splitter.setLayoutData(gd);
+    splitter.setOrientation(SWT.HORIZONTAL);
+
+    hotspotViewer = new TableViewer(splitter, SWT.H_SCROLL | SWT.V_SCROLL);
+
+    final Table table = hotspotViewer.getTable();
+    table.setHeaderVisible(false);
+    table.setLinesVisible(false);
+
+    TableViewerColumn colSeverity = new TableViewerColumn(hotspotViewer, SWT.NONE);
+    colSeverity.getColumn().setWidth(200);
+    colSeverity.getColumn().setText("Severity");
+    colSeverity.setLabelProvider(new ColumnLabelProvider() {
+      @Override
+      public String getText(Object element) {
+        return null;
+      }
+    });
+
+    TableViewerColumn colMessage = new TableViewerColumn(hotspotViewer, SWT.NONE);
+    colMessage.getColumn().setWidth(200);
+    colMessage.getColumn().setText("Description");
+    colMessage.setLabelProvider(new ColumnLabelProvider() {
+      @Override
+      public String getText(Object element) {
+        return ((ServerHotspot) element).message;
+      }
+    });
+
+    hotspotViewer.setContentProvider(ArrayContentProvider.getInstance());
+
+    hotspotViewer.setInput(new ServerHotspot[0]);
+
+    return form;
+  }
+
+  public void openHotspot(ServerHotspot hotspot) {
+    book.showPage(hotspotsPage);
+    hotspotViewer.setInput(new ServerHotspot[] {hotspot});
+    hotspotViewer.refresh();
+    splitter.layout();
   }
 
   @Override
   public void setFocus() {
+    book.forceFocus();
   }
 
   @Override
