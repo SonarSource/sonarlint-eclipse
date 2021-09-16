af22d5b2fbb8589fc753d2f4c86842601d56cacf
Julien HENRY <julien.henry@sonarsource.com>
2021-09-16 16:47:51 +0200

    SLE-470 Add a basic medium test for quick fixes

---
+++ org.sonarlint.eclipse.core.tests/testdata/SimpleJdtProject/src/main/java/com/quickfix/FileWithQuickFixes.java
@@ -0,0 +1,11 @@
+package com.quickfix;
+
+import java.util.*;
+
+public class FileWithQuickFixes {
+
+  public int foo(boolean a, String foo) {
+    List<String> list = new ArrayList<String>();
+    return 0;
+  }
+}
