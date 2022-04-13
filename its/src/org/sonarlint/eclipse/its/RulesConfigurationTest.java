b692248533a188e1dc801728ef1d26d7f53dcafc
Damien Urruty <damien.urruty@sonarsource.com>
2022-04-13 22:53:26 +0200

    Remove uneffective fix for IT

--- its/src/org/sonarlint/eclipse/its/RulesConfigurationTest.java
+++ its/src/org/sonarlint/eclipse/its/RulesConfigurationTest.java
@@ -141,8 +141,6 @@ public class RulesConfigurationTest extends AbstractSonarLintTest {
   private static void lowerCognitiveComplexityRuleParameter() {
     var ruleConfigurationPreferences = selectCognitiveComplexityRule();
     ruleConfigurationPreferences.setRuleParameter(10);
-    // Call apply before ok, else an analysis can be triggered before changed rules are saved
-    ruleConfigurationPreferences.apply();
     ruleConfigurationPreferences.ok();
   }
 
