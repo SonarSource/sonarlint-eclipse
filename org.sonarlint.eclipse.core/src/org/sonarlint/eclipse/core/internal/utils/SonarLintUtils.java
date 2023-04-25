cdb669f9e0a9b383bbd2432bda7c9a583f327bae
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2023-04-25 13:56:38 +0200

    SLE-610: Disable IPython
    IPython support was added in a recent version of the Python analyzer, but it's currently not supported in Eclipse.

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/utils/SonarLintUtils.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/utils/SonarLintUtils.java
@@ -97,7 +97,8 @@ public class SonarLintUtils {
   }
 
   public static Set<Language> getEnabledLanguages() {
-    var languagesDisabledByDefault = EnumSet.of(Language.JAVA, Language.CPP, Language.C, Language.OBJC, Language.SWIFT, Language.CS);
+    var languagesDisabledByDefault = EnumSet.of(Language.JAVA, Language.CPP, Language.C, Language.OBJC, Language.SWIFT,
+    											Language.CS, Language.IPYTHON);
     var enabledLanguages = EnumSet.complementOf(languagesDisabledByDefault);
     var configurators = SonarLintExtensionTracker.getInstance().getAnalysisConfigurators();
     for (var configurator : configurators) {
