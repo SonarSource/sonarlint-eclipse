7e752de1a3ed60274b96228c80caa891e2af5a48
Julien HENRY <julien.henry@sonarsource.com>
2023-05-25 09:07:17 +0200

    SLE-637 Fix invalid links to the smart notifications documentation

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/binding/wizard/connection/NotificationsWizardPage.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/binding/wizard/connection/NotificationsWizardPage.java
@@ -96,8 +96,8 @@ public class NotificationsWizardPage extends WizardPage {
       final var isSc = model.getConnectionType() == ConnectionType.SONARCLOUD;
       final var sqOrSc = isSc ? "SonarCloud" : "SonarQube";
       notificationsEnabledCheckbox.setText("Receive notifications from " + sqOrSc);
-      final var docUrl = isSc ? "https://sonarcloud.io/documentation/user-guide/sonarlint-notifications/"
-        : "https://docs.sonarqube.org/latest/user-guide/sonarlint-notifications/";
+      final var docUrl = isSc ? "https://docs.sonarcloud.io/advanced-setup/sonarlint-smart-notifications/"
+        : "https://docs.sonarqube.org/latest/user-guide/sonarlint-connected-mode/#smart-notifications";
       notificationsLink.setText("You will receive <a href=\"" + docUrl + "\">notifications</a> from " + sqOrSc + " in situations like:");
       notificationsDetails.setText(
         "  - the Quality Gate status of a bound project changes\n" +
