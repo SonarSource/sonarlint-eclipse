4bb75e813273a57c928e81a350918276c76ae279
Julien HENRY <julien.henry@sonarsource.com>
2023-06-15 14:36:00 +0200

    Fix tests and ITs
    * Separate CDT UI code in a separate class to avoid problem in headless tests
    * Fix assertion for rule description now that content is splitted in multiple browsers

---
+++ org.sonarlint.eclipse.cdt/src/org/sonarlint/eclipse/cdt/internal/CdtUiUtils.java
@@ -0,0 +1,45 @@
+/*
+ * SonarLint for Eclipse
+ * Copyright (C) 2015-2023 SonarSource SA
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarlint.eclipse.cdt.internal;
+
+import org.eclipse.cdt.internal.ui.text.doctools.DocCommentOwnerManager;
+import org.eclipse.cdt.ui.CUIPlugin;
+import org.eclipse.cdt.ui.text.CSourceViewerConfiguration;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
+
+public class CdtUiUtils {
+
+  private CdtUiUtils() {
+
+  }
+
+  public static SourceViewerConfiguration sourceViewerConfiguration() {
+    var tools = CUIPlugin.getDefault().getTextTools();
+    return new CSourceViewerConfiguration(tools.getColorManager(),
+      CUIPlugin.getDefault().getCorePreferenceStore(), null, tools.getDocumentPartitioning());
+  }
+
+  public static IDocumentPartitioner documentPartitioner() {
+    // use workspace default for highlighting doc comments in compare viewer
+    var owner = DocCommentOwnerManager.getInstance().getWorkspaceCommentOwner();
+    return CUIPlugin.getDefault().getTextTools().createDocumentPartitioner(owner);
+  }
+}
