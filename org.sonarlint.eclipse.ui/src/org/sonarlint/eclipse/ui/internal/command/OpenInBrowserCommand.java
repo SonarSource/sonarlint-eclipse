873ad06541680c078f2a564434ec6f88bdfb329d
Julien HENRY <julien.henry@sonarsource.com>
2023-07-04 12:14:13 +0200

    SLE-666 SLE-671 Only show menu when issue is matched

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/command/OpenInBrowserCommand.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/command/OpenInBrowserCommand.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.Adapters;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.menus.UIElement;
 import org.sonarlint.eclipse.core.SonarLintLogger;
@@ -40,13 +40,14 @@ public class OpenInBrowserCommand extends AbstractIssueCommand implements IEleme
 
   @Override
   public void updateElement(UIElement element, Map parameters) {
-    var window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-    if (window != null) {
-      var selection = (IStructuredSelection) window.getSelectionService().getSelection();
-      var binding = getBinding(getSelectedMarker(selection));
-      if (binding.isPresent()) {
-        element.setIcon(binding.get().getEngineFacade().isSonarCloud() ? SonarLintImages.SONARCLOUD_16 : SonarLintImages.SONARQUBE_16);
-      }
+    var window = element.getServiceLocator().getService(IWorkbenchWindow.class);
+    if (window == null) {
+      return;
+    }
+    var selection = (IStructuredSelection) window.getSelectionService().getSelection();
+    var binding = getBinding(getSelectedMarker(selection));
+    if (binding.isPresent()) {
+      element.setIcon(binding.get().getEngineFacade().isSonarCloud() ? SonarLintImages.SONARCLOUD_16 : SonarLintImages.SONARQUBE_16);
     }
   }
 
