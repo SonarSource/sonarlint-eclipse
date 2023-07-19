35083cbd79aae2009c61e398d132c8f9f9316e9e
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2023-07-19 17:30:19 +0200

    SLE-628: No bindings on technical projects
    Do not offer the binding of technical projects provided by PDE, PDT, RSE.

--- org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/resources/DefaultSonarLintProjectsProvider.java
+++ org.sonarlint.eclipse.core/src/org/sonarlint/eclipse/core/internal/resources/DefaultSonarLintProjectsProvider.java
@@ -26,19 +26,43 @@ import java.util.stream.Stream;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Adapters;
+import org.eclipse.core.runtime.Platform;
 import org.sonarlint.eclipse.core.resource.ISonarLintProject;
 import org.sonarlint.eclipse.core.resource.ISonarLintProjectsProvider;
 
 import static java.util.function.Predicate.not;
 
 public class DefaultSonarLintProjectsProvider implements ISonarLintProjectsProvider {
-
+  private static final String TECHNICAL_PROJECT_PDE = "External Plug-in Libraries";
+  private static final String TECHNICAL_PROJECT_PDT = "Servers";
+  private static final String TECHNICAL_PROJECT_RSE_1 = "RemoteSystemsTempFiles";
+  private static final String TECHNICAL_PROJECT_RSE_2 = "RemoteSystemsConnections";
+  
   @Override
   public Collection<ISonarLintProject> get() {
     return Stream.of(ResourcesPlugin.getWorkspace().getRoot().getProjects())
+      .filter(prj -> !isTechnicalProject(prj.getName()))
       .filter(IProject::isAccessible)
       .map(p -> Adapters.adapt(p, ISonarLintProject.class))
       .filter(not(Objects::isNull))
       .collect(Collectors.toList());
+  }
+  
+  /** This should be extended with further checks from other plug-ins providing technical projects */
+  private static boolean isTechnicalProject(String projectName) {
+    // check for Eclipse PDE technical projects
+    var isTechnicalProject = TECHNICAL_PROJECT_PDE.equals(projectName) && Platform.getBundle("org.eclipse.pde.ui") != null;
+    
+    // check for Eclipse PDT technical projects
+    isTechnicalProject = isTechnicalProject ||
+      (TECHNICAL_PROJECT_PDT.equals(projectName) && Platform.getBundle("org.eclipse.php.ui") != null);
+    
+    // check for Eclipse RSE technical projects
+    isTechnicalProject = isTechnicalProject ||
+      (TECHNICAL_PROJECT_RSE_1.equals(projectName) && Platform.getBundle("org.eclipse.rse.ui") != null);
+    isTechnicalProject = isTechnicalProject ||
+      (TECHNICAL_PROJECT_RSE_2.equals(projectName) && Platform.getBundle("org.eclipse.rse.ui") != null);
+    
+    return isTechnicalProject;
   }
 }
