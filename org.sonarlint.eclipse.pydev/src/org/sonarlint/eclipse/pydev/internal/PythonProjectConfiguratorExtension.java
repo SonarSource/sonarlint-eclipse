755ebfd0fc912cda914770d3a3aef61f0b572e10
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2023-11-02 15:16:11 +0100

    SLE-636: Python Syntax Highlighting
    In the Rule Description view / settings page we offer syntax highlighting only when the PyDev plug-in is available.
    
    Due to us hitting the timeout more and more it was increased to counter Cirrus CI flaky performance

---
+++ org.sonarlint.eclipse.pydev/src/org/sonarlint/eclipse/pydev/internal/PythonProjectConfiguratorExtension.java
@@ -0,0 +1,63 @@
+/*
+ * SonarLint for Eclipse
+ * Copyright (C) 2015-2023 SonarSource SA
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarlint.eclipse.pydev.internal;
+
+import java.util.Optional;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
+import org.sonarlint.eclipse.core.rule.ISyntaxHighlightingProvider;
+import org.sonarsource.sonarlint.core.commons.Language;
+
+public class PythonProjectConfiguratorExtension implements ISyntaxHighlightingProvider {
+  private final boolean pyDevPresent;
+
+  public PythonProjectConfiguratorExtension() {
+    pyDevPresent = isPyDevPresent();
+  }
+
+  private static boolean isPyDevPresent() {
+    return isClassPresentAtRuntime("org.python.pydev.plugin.PydevPlugin");
+  }
+
+  private static boolean isClassPresentAtRuntime(String className) {
+    try {
+      Class.forName(className);
+      return true;
+    } catch (ClassNotFoundException e) {
+      return false;
+    }
+  }
+
+  @Override
+  public Optional<SourceViewerConfiguration> sourceViewerConfiguration(String ruleLanguage) {
+    if (pyDevPresent && ruleLanguage.equals(Language.PYTHON.getLanguageKey())) {
+      return Optional.of(PyDevUtils.sourceViewerConfiguration());
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<IDocumentPartitioner> documentPartitioner(String ruleLanguage) {
+    if (pyDevPresent && ruleLanguage.equals(Language.PYTHON.getLanguageKey())) {
+      return Optional.of(PyDevUtils.documentPartitioner());
+    }
+    return Optional.empty();
+  }
+}
