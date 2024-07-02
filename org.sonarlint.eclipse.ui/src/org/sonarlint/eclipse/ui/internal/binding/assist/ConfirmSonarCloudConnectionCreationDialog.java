d9208f60ba667e5f6df5342335c73452e5727793
Tobias Hahnen <Tobias.hahnen@sonarsource.com>
2024-07-02 14:41:48 +0200

    SLE-884: Open in IDE from SonarCloud
    Some changes had to be done to the assistance of the connection creation in order to also work and warn properly for connections to SonarCloud.

---
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/binding/assist/ConfirmSonarCloudConnectionCreationDialog.java
@@ -0,0 +1,32 @@
+/*
+ * SonarLint for Eclipse
+ * Copyright (C) 2015-2024 SonarSource SA
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarlint.eclipse.ui.internal.binding.assist;
+
+import org.eclipse.swt.widgets.Shell;
+
+public class ConfirmSonarCloudConnectionCreationDialog extends AbstractConfirmConnectionCreationDialog {
+  public ConfirmSonarCloudConnectionCreationDialog(Shell parentShell, String organization, boolean automaticSetUp) {
+    super(parentShell,
+      "Do you trust SonarCloud?",
+      "SonarCloud is attempting to set up a connection for the organization '" + organization + "' with SonarLint.",
+      "Connect to SonarCloud",
+      automaticSetUp);
+  }
+}
