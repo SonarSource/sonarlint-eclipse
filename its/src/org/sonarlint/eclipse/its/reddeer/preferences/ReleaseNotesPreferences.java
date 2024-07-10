d3e7d98b19c9ae80f85f0b167b6f9f98bace100b
Tobias Hahnen <Tobias.hahnen@sonarsource.com>
2024-07-10 10:59:07 +0200

    SLE-863: Integration test for the Release Notes preferences
    Simple test to check that the Release Notes preferences is working correctly.

---
+++ its/src/org/sonarlint/eclipse/its/reddeer/preferences/ReleaseNotesPreferences.java
@@ -0,0 +1,62 @@
+/*
+ * SonarLint for Eclipse ITs
+ * Copyright (C) 2009-2024 SonarSource SA
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarlint.eclipse.its.reddeer.preferences;
+
+import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitUntil;
+import org.eclipse.reddeer.core.condition.WidgetIsFound;
+import org.eclipse.reddeer.eclipse.ui.dialogs.PropertyPage;
+import org.eclipse.reddeer.swt.api.Browser;
+import org.eclipse.reddeer.swt.condition.PageIsLoaded;
+import org.eclipse.reddeer.swt.impl.browser.InternalBrowser;
+import org.eclipse.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
+
+public class ReleaseNotesPreferences extends PropertyPage {
+  public ReleaseNotesPreferences(WorkbenchPreferenceDialog preferenceDialog) {
+    super(preferenceDialog, "SonarLint", "Release Notes");
+  }
+
+  public void cancel() {
+    ((WorkbenchPreferenceDialog) referencedComposite).cancel();
+  }
+
+  public Browser getFirstBrowser() {
+    // Browser can take a while to render
+    new WaitUntil(new WidgetIsFound(org.eclipse.swt.browser.Browser.class, getControl()),
+      TimePeriod.DEFAULT, false);
+    return new InternalBrowser(referencedComposite);
+  }
+
+  public String getFlatTextContent() {
+    new WaitUntil(new PageIsLoaded(getFirstBrowser()));
+    return getFirstBrowser().getText();
+  }
+
+  public static ReleaseNotesPreferences open() {
+    var preferenceDialog = new WorkbenchPreferenceDialog();
+    if (!preferenceDialog.isOpen()) {
+      preferenceDialog.open();
+    }
+
+    var releaseNotesPreferences = new ReleaseNotesPreferences(preferenceDialog);
+    preferenceDialog.select(releaseNotesPreferences);
+    return releaseNotesPreferences;
+  }
+}
