6a753f58fc5113eaf77ff9582f6f15034dbe9e9f
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-03-26 15:27:30 +0100

    SLE-1127 Update telemetry sample in the preferences

--- org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
+++ org.sonarlint.eclipse.ui/src/org/sonarlint/eclipse/ui/internal/properties/AboutPropertyPage.java
@@ -22,8 +22,7 @@ package org.sonarlint.eclipse.ui.internal.properties;
 import org.eclipse.jface.window.DefaultToolTip;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -32,7 +31,6 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PropertyPage;
 import org.sonarlint.eclipse.core.internal.preferences.SonarLintGlobalConfiguration;
 import org.sonarlint.eclipse.core.internal.telemetry.SonarLintTelemetry;
@@ -69,22 +67,31 @@ public class AboutPropertyPage extends PropertyPage implements IWorkbenchPrefere
     text.setText("By sharing anonymous SonarQube for Eclipse usage statistics, you help us understand how it is used so "
       + "we can improve the plugin to work even better for you.\nWe don't collect source code, IP addresses, or any personally identifying "
       + "information. And we don't share the data with anyone else.\nSee a <a href=\"#\">sample of the data.</a>");
-
-    final var tip = new DefaultToolTip(text, ToolTip.RECREATE, true);
+    
+    var sampleDataGd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+    var sampleDataLink = new Link(composite, SWT.UNDERLINE_LINK);
+    sampleDataLink.setForeground(sampleDataLink.getDisplay().getSystemColor(SWT.COLOR_BLUE));
+    sampleDataLink.setLayoutData(sampleDataGd);
+    sampleDataLink.setText("See a sample of data");
+    var handCursor = new Cursor(sampleDataLink.getDisplay(), SWT.CURSOR_HAND);
+    sampleDataLink.addListener(SWT.MouseEnter, event -> sampleDataLink.setCursor(handCursor));
+    sampleDataLink.addListener(SWT.MouseExit, event -> sampleDataLink.setCursor(null));
+     
+    final var tip = new DefaultToolTip(sampleDataLink, ToolTip.RECREATE, false);
     tip.setText("{\n"
       + "    \"days_since_installation\": 27,\n"
       + "    \"days_of_use\": 5,\n"
-      + "    \"sonarlint_version\": \"3.3.1\",\n"
+      + "    \"sonarlint_version\": \"11.3.0\",\n"
       + "    \"sonarlint_product\": \"SonarLint Eclipse\",\n"
       + "    \"ide_version\": \"Eclipse IDE 4.12.0.v20190605-1800\",\n"
       + "    \"connected_mode_used\": true,\n"
       + "    \"connected_mode_sonarcloud\": true,\n"
-      + "    \"system_time\":\"2018-02-27T16:31:49.173+01:00\",\n"
-      + "    \"install_time\":\"2018-02-01T16:30:49.124+01:00\",\n"
-      + "    \"analyses\":[{\"language\":\"java\",\"rate_per_duration\":{\"0-300\":100,\"300-500\":0,\"500-1000\":0,\"1000-2000\":0,\"2000-4000\":0,\"4000+\":0}}]\n,"
+      + "    \"system_time\":\"2025-03-26T11:45:44.513+01:00\",\n"
+      + "    \"install_time\":\"2024-01-12T10:49:17.196+01:00\",\n"
       + "    \"os\": \"Linux\",\n"
-      + "    \"jre\": \"11.0.6\",\n" +
-      "      \"nodejs\": \"11.12.0\",\n" +
+      + "    \"jre\": \"17.0.9\",\n" +
+      "      \"nodejs\": \"20.16.0\",\n" +
+      "      \"analyses\":[{\"language\":\"java\",\"rate_per_duration\":{\"0-300\":100,\"300-500\":0,\"500-1000\":0,\"1000-2000\":0,\"2000-4000\":0,\"4000+\":0}}]\n," +
       "      \"server_notifications\": {\n" +
       "            \"disabled\":false,\n" +
       "            \"count_by_type\":{\n" +
@@ -116,15 +123,30 @@ public class AboutPropertyPage extends PropertyPage implements IWorkbenchPrefere
       "                \"java:S1872\"\n" +
       "            ],\n" +
       "      }\n" +
+      "      \"hotspot\": {\n" +
+      "            \"open_in_browser_count\": 0,\n" +
+      "            \"status_changed_count\": 0\n" +
+      "      },\n" +
+      "      \"issue\": {\n" +
+      "            \"status_changed_rule_keys\": [],\n" +
+      "            \"status_changed_count\": 5\n" +
+      "      },\n" +
+      "      \"help_and_feedback\": {\n" +
+      "            \"count_by_link\": {},\n" +
+      "      },\n" +
+      "      \"cayc\": {\n" +
+      "            \"new_code_focus\": {\n" +
+      "                   \"enabled\": true\n" +
+      "                   \"changes\": 1\n" +
+      "            },\n" +
+      "      },\n" +
+      "      \"shared_connected_mode\": {\n" +
+      "            \"manual_bindings_count\": 1,\n" +
+      "            \"imported_bindings_count\": 0\n" +
+      "            \"auto_bindings_count\": 1\n" +
+      "            \"exported_connected_mode_count\": 1\n" +
+      "      },\n" +
       "}");
-
-    text.addSelectionListener(new SelectionAdapter() {
-      @Override
-      public void widgetSelected(SelectionEvent e) {
-        var cursorLocation = PlatformUI.getWorkbench().getDisplay().getCursorLocation();
-        tip.show(PlatformUI.getWorkbench().getDisplay().map(null, text, cursorLocation.x, cursorLocation.y));
-      }
-    });
 
     enabledBtn = new Button(composite, SWT.CHECK);
     enabledBtn.setText("Share anonymous SonarQube for Eclipse statistics");
