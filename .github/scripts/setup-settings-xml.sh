5cb84889d80ca4c1c2baaa5ce4a8563039a4ac37
Damien Urruty <damien.urruty@sonarsource.com>
2025-12-11 08:23:02 +0100

    Use different settings.xml files

--- .github/scripts/setup-settings-xml.sh
+++ .github/scripts/setup-settings-xml.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # Copy settings file
-cp .github/scripts/settings.xml "$HOME/.m2/settings.xml"
+cp .github/scripts/settings_${1-latest-java-11_e424}.xml "$HOME/.m2/settings.xml"
