f262169261efd6d760565af7e1d3831b8e49d44f
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2026-03-11 16:52:31 +0100

    Fix missing RSE

--- its/org.sonarlint.eclipse.its.standalone/src/org/sonarlint/eclipse/its/standalone/StandaloneAnalysisTest.java
+++ its/org.sonarlint.eclipse.its.standalone/src/org/sonarlint/eclipse/its/standalone/StandaloneAnalysisTest.java
@@ -33,6 +33,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.eclipse.condition.ProjectExists;
 import org.eclipse.reddeer.eclipse.jdt.ui.packageview.PackageExplorerPart;
@@ -498,6 +499,8 @@ public class StandaloneAnalysisTest extends AbstractSonarLintTest {
     // INFO: It is flaky when running on top of the oldest Eclipse version but works fine in the other test cases,
     // therefore it should be skipped in that particular situation!
     Assume.assumeTrue(!"oldest-java-11_e417".equals(System.getProperty("target.platform")));
+    // INFO: RSE was removed from Eclipse SimRel starting with 2026-03, skip when not installed
+    Assume.assumeTrue(Platform.getBundle("org.eclipse.rse.ui") != null);
 
     var remoteProjectDir = tempFolder.newFolder();
     FileUtils.copyDirectory(new File(projectDirectory, "java/java-simple"), remoteProjectDir);
